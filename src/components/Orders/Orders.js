6de5a06abfb76a8aa51182c983f4c0aaa9f3206e
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-22 10:18:15 -0600

    Move functionality for setting orders to Orders component

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setOrders } from '../../actions';
+import { getOrders } from '../../apiCalls';
 import './Orders.css';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
+class Orders extends Component {
+
+  componentDidMount() {
+    getOrders()
+      .then(data => this.props.setOrders(data.orders))
+      .catch(err => console.error('Error fetching:', err))
+  }
+
+  orderEls = () => this.props.orders.map(order => {
     return (
       <div className="order">
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
+            return <li> {ingredient}</li>
           })}
         </ul>
       </div>
     )
   });
 
-  return (
-    <section>
-      { orderEls.length ? orderEls : <p>No orders yet!</p> }
-    </section>
-  )
+  render() {
+    return (
+      <section>
+          { this.props.orders.length ? this.orderEls() : <p>No orders yet!</p> }
+      </section>
+    )
+  }
 }
 
-export default Orders;
+const mapStateToProps = ({ orders }) => ({
+  orders
+});
+
+const mapDispatchToProps = dispatch => (
+  {
+    setOrders: (orders) => dispatch(setOrders(orders))
+  }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);
